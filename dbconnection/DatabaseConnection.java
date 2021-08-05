812a2e09115c680e964c3bf942c507519621e63c
nguyentung36 <tungpntgcs17645@fpt.edu.vn>
2021-08-05 21:34:15 +0700

    Initial commit

---
+++ dbconnection/DatabaseConnection.java
@@ -0,0 +1,6 @@
+package cybersoft.java12.springdemo.dbconnection;
+
+public interface DatabaseConnection {
+	void getConnection();
+
+}
---
+++ model/Author.java
@@ -0,0 +1,28 @@
+package cybersoft.java12.springdemo.model;
+
+public class Author {
+	private String name;
+	private String email;
+	private String facebook;
+	
+	public Author() {
+		name = "Tung Phan";
+		email = "nguyentung361998@gmail.com";
+		facebook = "https://www.facebook.com/pnt.tung";
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getFacebook() {
+		return facebook;
+	}
+	
+	
+
+}
